15a6f88d36b64919ad38322f9bae9e1e9b0c1a2b
qingyang <qingyang_88@sina.com>
2022-11-06 22:29:34 +0800

    feat: weixin api

--- index.js
+++ index.js
@@ -1,67 +1,94 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const logger = require("koa-logger");
-const bodyParser = require("koa-bodyparser");
-const fs = require("fs");
-const path = require("path");
-const { init: initDB, Counter } = require("./db");
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const fs = require('fs');
+const path = require('path');
+const { init: initDB, Counter } = require('./db');
+const request = require('request');
+const qs = require('qs');
 
 const router = new Router();
 
-const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
+const homePage = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
 
 // 首页
-router.get("/", async (ctx) => {
-  ctx.body = homePage;
+router.get('/', async (ctx) => {
+	ctx.body = homePage;
+});
+
+// 获取广告收益明细
+
+router.get('/ad/publisher_adunit_general', async (ctx) => {
+	const params = qs.stringify({
+		page: 1,
+		page_size: 10,
+		start_date: '20221030',
+		end_date: '20221030',
+	});
+	const res = await new Promise((resolve, reject) => {
+		request(
+			{
+				method: 'get',
+				url: `https://api.weixin.qq.com/publisher/stat?action=publisher_adunit_general&${params}`,
+			},
+			function (error, response) {
+				resolve(JSON.parse(response.body));
+			},
+		);
+	});
+	ctx.body = {
+		res,
+	};
 });
 
 // 更新计数
-router.post("/api/count", async (ctx) => {
-  const { request } = ctx;
-  const { action } = request.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
+router.post('/api/count', async (ctx) => {
+	const { request } = ctx;
+	const { action } = request.body;
+	if (action === 'inc') {
+		await Counter.create();
+	} else if (action === 'clear') {
+		await Counter.destroy({
+			truncate: true,
+		});
+	}
 
-  ctx.body = {
-    code: 0,
-    data: await Counter.count(),
-  };
+	ctx.body = {
+		code: 0,
+		data: await Counter.count(),
+	};
 });
 
 // 获取计数
-router.get("/api/count", async (ctx) => {
-  const result = await Counter.count();
+router.get('/api/count', async (ctx) => {
+	const result = await Counter.count();
 
-  ctx.body = {
-    code: 0,
-    data: result,
-  };
+	ctx.body = {
+		code: 0,
+		data: result,
+	};
 });
 
 // 小程序调用，获取微信 Open ID
-router.get("/api/wx_openid", async (ctx) => {
-  if (ctx.request.headers["x-wx-source"]) {
-    ctx.body = ctx.request.headers["x-wx-openid"];
-  }
+router.get('/api/wx_openid', async (ctx) => {
+	if (ctx.request.headers['x-wx-source']) {
+		ctx.body = ctx.request.headers['x-wx-openid'];
+	}
 });
 
 const app = new Koa();
 app
-  .use(logger())
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
+	.use(logger())
+	.use(bodyParser())
+	.use(router.routes())
+	.use(router.allowedMethods());
 
 const port = process.env.PORT || 80;
 async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
+	await initDB();
+	app.listen(port, () => {
+		console.log('启动成功', port);
+	});
 }
 bootstrap();
